ce243a2ef354aa19be1843f04b160d5317e645ba
Rohith Gundaram <87968757+RohithGundaram@users.noreply.github.com>
2023-10-03 21:43:55 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,10 @@
-def caesar_cipher(text, shift):
-    result = ""
-
-    for char in text:
-        if char.isalpha():  # Check if the character is a letter
-            is_upper = char.isupper()  # Check if it's uppercase
-            offset = ord('A' if is_upper else 'a')
-            result += chr(((ord(char) - offset + shift) % 26) + offset)
-        else:
-            result += char  # Keep non-alphabetic characters unchanged
-
-    return result
-
-# Example usage
-plaintext = "Hello, World!"
-shift = 3  # You can change the shift value as needed
-print("Plain Tet:", plaintext)
-encrypted_text = caesar_cipher(plaintext, shift)
+from pycipher import Caesar
+plaintext = "Instrument"     
+print("Plain text:",plaintext)
+shift=3
+cipher = Caesar(shift)
+encrypted_text = cipher.encipher(plaintext)
 print("Encrypted:", encrypted_text)
 
-decrypted_text = caesar_cipher(encrypted_text, -shift)  # Decrypt by shifting back
-print("Decrypted:", decrypted_text)
+decrypted_text = cipher.decipher(encrypted_text)
+print("Decrypted:",decrypted_text)
